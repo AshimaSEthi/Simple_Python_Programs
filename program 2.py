9a079f203447dcf3c05d3e610a0deeaedecc6a52
AshimaSEthi <sethiashima33@gmail.com>
2021-05-12 21:59:53 +0530

    Addition In Python

---
+++ program 2.py
@@ -0,0 +1,11 @@
+a = 25
+b = 50
+
+sum = a+b
+print("The sum of two Number is :",sum)
+
+num1 = int(input("Enter first Number: "))4
+num2 = int(input("Enter second Number: "))
+
+sum = num1+num2
+print("The sum of two number is :", sum)
