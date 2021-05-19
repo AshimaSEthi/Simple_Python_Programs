9eb1c6bef48b2531981b686280063019aa6cb263
AshimaSEthi <sethiashima33@gmail.com>
2021-05-19 08:00:33 +0530

    Sqaure and Square Root

---
+++ program 3.py
@@ -0,0 +1,17 @@
+import math
+
+# To give Value In Code
+num = 16
+
+#To Take input from the user
+enter_num = int(input("Enter The Number: "))
+result = math.sqrt(enter_num)
+print("The Square Root is : ",result)
+
+#To Calculate Power of a Number
+
+Num = int(input("Enter The Number"))
+
+#To find the cube root of a num, if you want to find square then replace 3 with 2
+result_pow = math.pow(Num,3)
+print("The Square of a Number is: ",result_pow)
